5ee99d8cd9cca097bcd08bd1c4f0e38a8e3a9ce1
Maria Ocampo <mariaocampo0513@gmail.com>
2020-11-07 19:21:37 -0800

    created rows inside the main div.with time, description, and save buttton

---
+++ Develop/js/script.js
@@ -0,0 +1,24 @@
+let m = moment()
+var currentDays = moment().format("dddd, MMM Do YYYY");
+var hours =  ['8', '9', '10', '11', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+AMPM = ''
+
+
+
+
+// 
+//     WHEN I open the planner
+//     THEN the current day is displayed at the top of the calendar
+
+//     WHEN I scroll down
+//     THEN I am presented with time blocks for standard business hours
+
+//     WHEN I view the time blocks for that day
+//     THEN each time block is color-coded to indicate whether it is in the past, present, or future
+
+//     WHEN I click into a time block
+//     THEN I can enter an event
+//     WHEN I click the save button for that time block
+//     THEN the text for that event is saved in local storage
+//     WHEN I refresh the page
+//     THEN the saved events persist -->
--- Develop/script.js
+++
@@ -1,5 +0,0 @@
-var currentDayOfYear = moment().dayOfYear()
-var newDayOfYear = currentDayOfYear
-var nextDayOfYear
-var prevDayOfYear
-var hours =  ['7', '8', '9', '10', '11', '12', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
