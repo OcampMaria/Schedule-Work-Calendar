9338f5208ae093a386428cc2b38efae6e3c2ac4d
Maria Ocampo <mariaocampo0513@gmail.com>
2020-11-07 16:27:14 -0800

    created rows inside the main div.with time, description, and save buttton

---
+++ Develop/script.js
@@ -0,0 +1,5 @@
+var currentDayOfYear = moment().dayOfYear()
+var newDayOfYear = currentDayOfYear
+var nextDayOfYear
+var prevDayOfYear
+var hours =  ['7', '8', '9', '10', '11', '12', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
